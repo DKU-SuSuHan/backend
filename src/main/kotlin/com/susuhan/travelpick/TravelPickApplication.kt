9ce7da63d99ff3299dc6382dffe0746dc8f4f275
sujeong11 <qalzm7351@gmail.com>
2024-04-07 14:20:06 +0900

    Initial commit

---
+++ src/main/kotlin/com/susuhan/travelpick/TravelPickApplication.kt
@@ -0,0 +1,11 @@
+package com.susuhan.travelpick
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelPickApplication
+
+fun main(args: Array<String>) {
+	runApplication<TravelPickApplication>(*args)
+}
---
+++ src/test/kotlin/com/susuhan/travelpick/TravelPickApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.susuhan.travelpick
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TravelPickApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
