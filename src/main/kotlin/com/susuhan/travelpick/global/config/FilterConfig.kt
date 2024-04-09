cba3cc08d6d15cf0d72688ff72ec4db6e632e749
sujeong11 <qalzm7351@gmail.com>
2024-04-10 01:22:52 +0900

    feat: #3 로그를 남길 filter 생성 및 등록

---
+++ src/main/kotlin/com/susuhan/travelpick/global/config/FilterConfig.kt
@@ -0,0 +1,23 @@
+package com.susuhan.travelpick.global.config
+
+import com.susuhan.travelpick.global.log.LogFilter
+import jakarta.servlet.Filter
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfig {
+
+    @Bean
+    fun logApiFilter(): FilterRegistrationBean<Filter> {
+        return FilterRegistrationBean<Filter>().apply {
+            this.filter = LogFilter()
+            this.order = -101
+            this.setName("LogFilter")
+            this.addUrlPatterns("/*")
+        }
+    }
+}
---
+++ src/main/kotlin/com/susuhan/travelpick/global/log/ResponseWrapper.kt
@@ -0,0 +1,7 @@
+package com.susuhan.travelpick.global.log
+
+import jakarta.servlet.http.HttpServletResponse
+
+import org.springframework.web.util.ContentCachingResponseWrapper
+
+class ResponseWrapper(response: HttpServletResponse) : ContentCachingResponseWrapper(response)
