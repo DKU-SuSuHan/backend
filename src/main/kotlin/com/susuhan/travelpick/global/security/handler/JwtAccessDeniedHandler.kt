90ead711c9c22d31ab95f8e604fd7599f162c4cb
sujeong11 <qalzm7351@gmail.com>
2024-04-12 11:19:10 +0900

    feat: #9 logger를 생성하는 팩토리 메서드 생성

--- src/main/kotlin/com/susuhan/travelpick/global/security/handler/JwtAccessDeniedHandler.kt
+++ src/main/kotlin/com/susuhan/travelpick/global/security/handler/JwtAccessDeniedHandler.kt
@@ -1,11 +1,11 @@
 package com.susuhan.travelpick.global.security.handler
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.susuhan.travelpick.global.common.logger
 import com.susuhan.travelpick.global.exception.ErrorCode
 import com.susuhan.travelpick.global.exception.dto.ErrorResponse
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.AccessDeniedException
@@ -17,15 +17,13 @@ import kotlin.jvm.Throws
 @Component
 class JwtAccessDeniedHandler : AccessDeniedHandler {
 
-    private val logger = LoggerFactory.getLogger(this.javaClass)
-
     @Throws(IOException::class)
     override fun handle(
         request: HttpServletRequest?,
         response: HttpServletResponse?,
         accessDeniedException: AccessDeniedException?
     ) {
-        logger.error("[Access_Denied_Handler]: 사용자가 필요한 권한이 없는 상태로 접근했습니다.");
+        logger().error("[Access_Denied_Handler]: 사용자가 필요한 권한이 없는 상태로 접근했습니다.");
 
         response?.status = HttpStatus.FORBIDDEN.value()
         response?.contentType = MediaType.APPLICATION_JSON_VALUE
