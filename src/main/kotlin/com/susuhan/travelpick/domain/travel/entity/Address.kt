d0851bc2a5640ccb6a5a1a63ac45ae28a9b45631
sujeong11 <qalzm7351@gmail.com>
2024-05-05 18:45:59 +0900

    style: #18 주석에 띄어쓰기 추가

--- src/main/kotlin/com/susuhan/travelpick/domain/travel/entity/Address.kt
+++ src/main/kotlin/com/susuhan/travelpick/domain/travel/entity/Address.kt
@@ -4,6 +4,6 @@ import jakarta.persistence.Embeddable
 
 @Embeddable
 data class Address(
-    var sido: String, // 행정구역의 시, 도
-    var sgg: String // 행정구역의 시, 군, 구
+    var sido: String, // 행정 구역의 시, 도
+    var sgg: String // 행정 구역의 시, 군, 구
 )
