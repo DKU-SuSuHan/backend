bd681c0bc6477cc9fe2373f31a099d30845a3df2
sujeong11 <qalzm7351@gmail.com>
2024-05-05 18:44:10 +0900

    refactor: #18 불필요한 코드 제거

--- src/main/kotlin/com/susuhan/travelpick/domain/user/dto/request/NicknameCheckRequest.kt
+++ src/main/kotlin/com/susuhan/travelpick/domain/user/dto/request/NicknameCheckRequest.kt
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size
 
 data class NicknameCheckRequest(
     @Schema(description = "중복 여부를 조회할 닉네임")
-    @field:NotBlank(message = "닉네임은 공백이나 빈 값이 아니여야 합니다.")
+    @field:NotBlank
     @field:Size(max = 10, message = "닉네임은 10자 이하여야 합니다.")
     val nickname: String
 )
