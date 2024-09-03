c1152230bf589c509df23a816b8c01df09be79a1
sujeong11 <qalzm7351@gmail.com>
2024-09-03 13:34:50 +0900

    test: #110 request dto가 KotlinFixture로 생성되도록 수정

--- src/test/kotlin/com/susuhan/travelpick/domain/auth/service/AuthCommandServiceTest.kt
+++ src/test/kotlin/com/susuhan/travelpick/domain/auth/service/AuthCommandServiceTest.kt
@@ -1,5 +1,6 @@
 package com.susuhan.travelpick.domain.auth.service
 
+import com.susuhan.travelpick.KotlinFixture
 import com.susuhan.travelpick.domain.auth.dto.KakaoUserInfo
 import com.susuhan.travelpick.domain.auth.dto.request.KakaoLoginRequest
 import com.susuhan.travelpick.domain.auth.dto.response.TokenResponse
@@ -21,16 +22,16 @@ class AuthCommandServiceTest : BehaviorSpec({
     val sut = AuthCommandService(kakaoApiClient, jwtTokenService, userRepository)
 
     Given("카카오 로그인이 처음인 사용자가") {
-        val request = KakaoLoginRequest("kakao-access-token")
+        val request = KotlinFixture.fixture<KakaoLoginRequest>()
+        val expectResult = KotlinFixture.fixture<TokenResponse>()
         val kakaoAccessToken = "Bearer ${request.kakaoAccessToken}"
         val kakaoUserInfo = KakaoUserInfo(Random.nextInt().toString(), null, null, null)
         val user = kakaoUserInfo.toEntity()
-        val tokenResponse = TokenResponse("access-token", "refresh-token")
 
         every { kakaoApiClient.getUserInfo(kakaoAccessToken) } returns kakaoUserInfo
         every { userRepository.findNotDeletedUserBySocialId(kakaoUserInfo.id) } returns null
         every { userRepository.save(any()) } returns user
-        every { jwtTokenService.createJwtTokens(user) } returns tokenResponse
+        every { jwtTokenService.createJwtTokens(user) } returns expectResult
 
         When("카카오 로그인을 요청하면") {
             val result = sut.login(request)
@@ -41,23 +42,22 @@ class AuthCommandServiceTest : BehaviorSpec({
                 verify(exactly = 1) { userRepository.save(any()) }
                 verify(exactly = 1) { jwtTokenService.createJwtTokens(user) }
             }
-            Then("토큰을 생성해 반환한다.") {
-                result.accessToken shouldBe tokenResponse.accessToken
-                result.refreshToken shouldBe tokenResponse.refreshToken
+            Then("토큰을 생성해 반환해야 한다.") {
+                result shouldBe expectResult
             }
         }
     }
 
     Given("이전에 카카오 로그인을 했던 사용자가") {
-        val request = KakaoLoginRequest("kakao-access-token2")
+        val request = KotlinFixture.fixture<KakaoLoginRequest>()
+        val expectResult = KotlinFixture.fixture<TokenResponse>()
         val kakaoAccessToken = "Bearer ${request.kakaoAccessToken}"
         val kakaoUserInfo = KakaoUserInfo(Random.nextInt().toString(), null, null, null)
         val user = kakaoUserInfo.toEntity()
-        val tokenResponse = TokenResponse("access-token", "refresh-token")
 
         every { kakaoApiClient.getUserInfo(kakaoAccessToken) } returns kakaoUserInfo
         every { userRepository.findNotDeletedUserBySocialId(kakaoUserInfo.id) } returns user
-        every { jwtTokenService.createJwtTokens(user) } returns tokenResponse
+        every { jwtTokenService.createJwtTokens(user) } returns expectResult
 
         When("카카오 로그인을 요청하면") {
             val result = sut.login(request)
@@ -68,9 +68,8 @@ class AuthCommandServiceTest : BehaviorSpec({
                 verify(exactly = 0) { userRepository.save(kakaoUserInfo.toEntity()) }
                 verify(exactly = 1) { jwtTokenService.createJwtTokens(user) }
             }
-            Then("토큰을 생성해 반환한다.") {
-                result.accessToken shouldBe tokenResponse.accessToken
-                result.refreshToken shouldBe tokenResponse.refreshToken
+            Then("토큰을 생성해 반환해야 한다.") {
+                result shouldBe expectResult
             }
         }
     }
