99aefe8b4e53a54c4d20b24f02fce6c04bd7d55f
sujeong11 <qalzm7351@gmail.com>
2024-09-08 12:57:28 +0900

    test: #114 테스트 프로파일 활성화 어노테이션 추가

--- src/test/kotlin/com/susuhan/travelpick/domain/travel/service/TravelQueryServiceTest.kt
+++ src/test/kotlin/com/susuhan/travelpick/domain/travel/service/TravelQueryServiceTest.kt
@@ -14,7 +14,9 @@ import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("test")
 class TravelQueryServiceTest : BehaviorSpec({
 
     val travelMateRepository = mockk<TravelMateRepository>()
